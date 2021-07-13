ad24dc860d1477aa38a259efca57c034d0326f56
lqq-tj <1363276408@qq.com>
2021-07-13 20:19:42 +0800

    Track and position fields are added

--- script/name_title.py
+++ script/name_title.py
@@ -4,10 +4,11 @@
 from pypinyin import lazy_pinyin
 import openpyxl
 import csv
+from translate import Translator
  
 # First, define a CSV file
-zh_head =["zh_name","zh_title"]
-en_head =["en_name","en_title"]
+zh_head =["zh_name","zh_title","track","zh_position"]
+en_head =["en_name","en_title","track","en_position"]
 with open("./zh_name_title.csv","a+",encoding="utf-8",newline="") as zh_f:
    csvf=csv.writer(zh_f)
    csvf.writerow(zh_head)
@@ -49,7 +50,6 @@ def translateTitle_ch_en(zh_title):
 # Gets the active table object
 session_active=session.active
 collect_active=collect.active
-
 flag=False
 for cell1 in session_active['J']:
    str=cell1.value
@@ -62,6 +62,7 @@ for cell1 in session_active['J']:
          names=i.split(" <")
          zh_title=session_active.cell(row,7).value
          en_title=session_active.cell(row,6).value
+         track=session_active.cell(row,1).value
          zh_name=names[0].strip()
          # For names with brackets, remove the brackets and the contents in the brackets
          zh_name=zh_name.split('(',1)[0]
@@ -76,10 +77,27 @@ for cell1 in session_active['J']:
                for mail in collect_active['J'] :
                   if mail.value==session_mail :
                      zh_name=collect_active.cell(mail.row,3).value
+                     break
+            
+            # According to the topic of Chinese speech, get the position information
+            flag=False
+            for position in collect_active['D']:
+               if position.value==zh_title or position.value==en_title:
+                  zh_position=collect_active.cell(position.row,6).value
+
+                  # 中文翻译成英文
+                  translator=Translator(from_lang="chinese",to_lang="english")
+                  en_position=translator.translate(zh_position)
+                  flag=True
+                  break
+            if flag==False :
+               zh_position=0
+               en_position=0
+            
 
             # Write data to a CSV file
-            zh_data=[(zh_name,zh_title)]
-            en_data=[(en_name,en_title)]
+            zh_data=[(zh_name,zh_title,track,zh_position)]
+            en_data=[(en_name,en_title,track,en_position)]
             with open("./zh_name_title.csv","a+",encoding="utf-8",newline="") as zh_f:
                csvf=csv.writer(zh_f)
                csvf.writerows(zh_data)
